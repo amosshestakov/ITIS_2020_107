b8d5522acc75cf27abfe09c160be9ffe7c56759f
Ilya Lapin <lapinilya2@mail.ru>
2020-02-17 10:51:39 +0300

    initial

---
+++ IdeaProjects/sem2/src/ru/kpfu/itis/group907/lapin/algorithms/pair1/LinkedList.java
@@ -0,0 +1,8 @@
+package ru.kpfu.itis.group907.lapin.algorithms.pair1;
+
+public class LinkedList {
+    LinkedList next;
+    int data;
+
+
+}
---
+++ IdeaProjects/sem2/src/ru/kpfu/itis/group907/lapin/algorithms/pair1/Node.java
@@ -0,0 +1,13 @@
+package ru.kpfu.itis.group907.lapin.algorithms.pair1;
+
+public class Node {
+    private Node next;
+    private int data;
+
+    Node( int data, Node node ) {
+        this.data = data;
+        this.next = node;
+    }
+
+
+}
